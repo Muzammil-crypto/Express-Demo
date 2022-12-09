5d061a06b895c2e6d3d6c8899f69f565fc44b614
FIRST_NAME LAST_NAME <57823963+Muzammil-crypto@users.noreply.github.com>
2022-12-09 12:54:13 +0500

    Envoirment Variable added

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Word");
+app.get("/", (_req, res) => {
+  res.send("Hello World");
 });
 app.get("/api/courses", (req, res) => {
   res.send([1, 2, 3]);
 });
-app.listen(3000, () => console.log("Listning on port 3000"));
 
-// App.post()
-// App.put()
-// App.delete()
+app.listen(port, () => {
+  console.log(`Server running on port: `, port);
+});
