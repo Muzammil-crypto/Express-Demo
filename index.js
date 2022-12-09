30f0c09070d6d5db1ac79cd22dc1ecb6f6bc1c2e
FIRST_NAME LAST_NAME <57823963+Muzammil-crypto@users.noreply.github.com>
2022-12-09 12:06:41 +0500

    response sent

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
-const App = express();
+const app = express();
 
-App.get("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("Hello Word");
 });
+app.get("/api/courses", (req, res) => {
+  res.send([1, 2, 3]);
+});
+app.listen(3000, () => console.log("Listning on port 3000"));
 
 // App.post()
 // App.put()
